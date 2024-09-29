a9539a0a126aa5f23dc1140d4515fc7c133cf9c5
Michael <elekwachi50@gmail.com>
2024-09-29 14:04:54 -0700

    redis.js working on sandbox

--- utils/redis.js
+++ utils/redis.js
@@ -1,33 +1,49 @@
-import { createClient } from "redis";
+const redis = require('redis');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
-
+    this.client = redis.createClient();
     this.client.on("error", (error) => {
       console.error(`Redis client error: ${error}`);
     });
 
-    // Connect to Redis
-    this.client.connect().catch(console.error);
+    this.promiseGet = promisify(this.client.get).bind(this.client);
+    this.promiseDel = promisify(this.client.del).bind(this.client);
+    this.promiseSet = promisify(this.client.set).bind(this.client);
   }
 
   isAlive() {
-    return this.client.isOpen;
+    return this.client.connected;
   }
 
   async get(key) {
-    return this.client.get(key);
+    try {
+      return await this.promiseGet(key);
+    } catch (err) {
+      console.error(`Error getting key ${key}: ${err}`);
+      throw err;
+    }
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value, {
-      EX: duration,
-    });
+  try {
+    // Redis expects 'EX' and the expiration time as separate arguments.
+    return await this.promiseSet(key, JSON.stringify(value), 'EX', duration);
+  } catch (err) {
+    console.error(`Error setting key ${key}: ${err}`);
+    throw err;
   }
+}
+
 
   async del(key) {
-    await this.client.del(key);
+    try {
+      await this.promiseDel(key);
+    } catch (error) {
+      console.error('Error in del method', error.message);
+      throw error;
+    }
   }
 }
 
---
+++ utils/redis1.js
@@ -0,0 +1,120 @@
+/*//import { createClient } from "redis"; --ES6 way of calling redis but using commonjs
+
+const redis = require('redis');
+
+/**const utilities = require('util');
+const promisify = utilities.promisify(); -Tried to use this in the same manner as declaring redis client
+					- But utilities.promisify() is not a function that returns a promisify function. Instead, utilities.promisify is a function that takes a function as an argument and returns a promisified version of that function.
+*/
+
+/* const { promisify } = require('util') // -this works looks like es6 + commonjs
+
+
+class RedisClient {
+  constructor() {
+    //this.client = createClient();
+    this.client = redis.createClient();
+    this.client.on("error", (error) => {
+      console.error(`Redis client error: ${error}`);
+    });
+
+    // Connect to Redis
+    //this.client.connect().catch(console.error);
+    //await this.client.connect(); - used in later versions of redis but currently connects automatically
+
+
+  //Creating promiseversions of the different redis functions
+/*  this.promiseGet = promisify(this.client.get).bind(this.client);
+  this.promiseDel = promisify(this.client.del).bind(this.client);
+  this.promiseSet = promisify(this.client.del.toString()).bind(this.client);
+}
+
+  isAlive() {
+    return this.client.connected && this.client.ready;
+  }
+
+  async get(key) {
+    try {
+	return await this.promiseGet(key);
+   } catch (err) {
+    console.error(`Error getting key ${key}: ${err}`);
+    throw err;
+   }
+}
+
+  async set(key, value, duration) {
+    try {
+      	return await this.promiseSet(key, JSON.stringify(value), {
+      	EX: duration,
+    });
+  } catch(err) {
+	console.eror(`Error setting key ${key}: ${err}`)
+	throw err;
+	}
+  }
+
+  async del(key) {
+   try { await this.promiseDel(key);
+  } catch (error) {
+	console.error('Error in del method', error.message);
+	throw error;
+	}
+  }
+}
+
+
+const redisClient = new RedisClient();
+export default redisClient;
+*/
+
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on("error", (error) => {
+      console.error(`Redis client error: ${error}`);
+    });
+
+    this.promiseGet = promisify(this.client.get).bind(this.client);
+    this.promiseDel = promisify(this.client.del).bind(this.client);
+    this.promiseSet = promisify(this.client.set).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected && this.client.ready;
+  }
+
+  async get(key) {
+    try {
+      return await this.promiseGet(key);
+    } catch (err) {
+      console.error(`Error getting key ${key}: ${err}`);
+      throw err;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      return await this.promiseSet(key, JSON.stringify(value), {
+        EX: duration,
+      });
+    } catch (err) {
+      console.error(`Error setting key ${key}: ${err}`);
+      throw err;
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.promiseDel(key);
+    } catch (error) {
+      console.error('Error in del method', error.message);
+      throw error;
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
